3ee24a4e30a4400847ef74f1c5e433ad8c044a9f
Nadine Bengtsson <nadine.bengtsson@gmail.com>
2021-02-04 00:06:50 -0300

    cambios - OnAdd no agrega

--- src/components/ItemCount/index.jsx
+++ src/components/ItemCount/index.jsx
@@ -1,12 +1,12 @@
 import "./style.css";
 
-export const ItemCount = ({stock, carrito, onAdd}) => {
+export const ItemCount = ({stock, carrito, onAdd, onSubstract, setCarrito, product}) => {
     return (
         <>
-             <button className="resta" onClick={ () => {onSubstract(carrito)}}> - </button>;
+            <button className="resta" onClick={() => {onSubstract(carrito)}}> - </button>
             <b>{carrito}</b>
-            <b> Tenemos en el stock{stock}</b>
-            <button className="suma" onClick={ () => {onAdd(carrito)}}> + </button>;
+            <p> Tenemos en el {stock} en stock</p>
+            <button className="suma" onClick={ () => {onAdd(carrito)}}> + </button>
             <button className="comprar"onClick= {() => {setCarrito([...carrito, product.name, product.description, product.price, product.image])}}> Agregar al carrito </button>
         </>
     );
--- src/container/index.jsx
+++ src/container/index.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState} from 'react';
 import { ItemCount } from '../components/ItemCount/index';
 import ProductCard from './ProductCard/index';
+import "./style.css";
 //import Product from './Product-List/index';
 
 //API - const [datos, setDatos] = useState(0)
@@ -17,21 +18,23 @@ import ProductCard from './ProductCard/index';
 
 //carrito
 
-const Products =({greeting}, stock, carrito, onAdd, onSubstract) => {
+const Products =({greeting}, stock) => {
 
     const [carrito, setCarrito] = useState()
     
     const onAdd= (carrito, stock) => {
         if (carrito < stock){
-            setCarrito(carrito + 1); alert("Producto agregado al carrito");} else {
+            setCarrito(carrito + 1); alert("Producto agregado al carrito");
+        } 
+        else {
                 alert("No hay stock")
-            }};
+        }};
 
-    const onSubstract = () => {
+    const onSubstract=(carrito) => {
         if (carrito > 1 ) {
             setCarrito(carrito - 1)
         } else {
-            alert("Opcion no disponible")
+            console.log("Opcion no disponible")
         }
     };
     
@@ -39,7 +42,7 @@ const Products =({greeting}, stock, carrito, onAdd, onSubstract) => {
         <> 
             <div className={greeting} />
             <h2> Tienda </h2>
-            <ItemCount stock={12} carrito={carrito} on onAdd={onAdd}/>
+            <ItemCount stock={12} carrito={0} on onAdd={onAdd} on onSubstract={onSubstract}/>
              <ProductCard />
         </>
     )};
