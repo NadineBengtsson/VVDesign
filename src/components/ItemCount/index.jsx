e9b9ac7fe2fa73e1d5be69bc46126db62278af73
Nadine Bengtsson <nadine.bengtsson@gmail.com>
2021-02-03 23:35:45 -0300

    mini cambio

--- src/components/ItemCount/index.jsx
+++ src/components/ItemCount/index.jsx
@@ -6,7 +6,7 @@ export const ItemCount = ({stock, carrito, onAdd}) => {
              <button className="resta" onClick={ () => {onSubstract(carrito)}}> - </button>;
             <b>{carrito}</b>
             <b> Tenemos en el stock{stock}</b>
-            <button className="suma" onClick={ () => {onAdd(stock)}}> + </button>;
+            <button className="suma" onClick={ () => {onAdd(carrito)}}> + </button>;
             <button className="comprar"onClick= {() => {setCarrito([...carrito, product.name, product.description, product.price, product.image])}}> Agregar al carrito </button>
         </>
     );
